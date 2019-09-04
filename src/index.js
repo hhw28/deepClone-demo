0846adb04036c15c820b891efd4fba5fc6c7b625
wanghuan <196912334@qq.com>
2019-09-04 22:56:48 +0800

    深拷贝基本类型，普通对象，数组对象和函数

---
+++ src/index.js
@@ -0,0 +1,21 @@
+function deepClone(source) {
+  if (source instanceof Object) {
+    let dist;
+    if (source instanceof Array) {
+      dist = new Array();
+    } else if (source instanceof Function) {
+      dist = function() {
+        return source.apply(this, arguments);
+      };
+    } else {
+      dist = new Object();
+    }
+    for (let key in source) {
+      dist[key] = deepClone(source[key]);
+    }
+    return dist;
+  }
+  return source;
+}
+
+module.exports = deepClone;
---
+++ test/index.js
@@ -0,0 +1,59 @@
+const chai = require("chai");
+const deepClone = require("../src/index");
+const assert = chai.assert;
+
+describe("chai的使用", () => {
+  it("deepClone是个函数", () => {
+    assert.isFunction(deepClone);
+  });
+  it("能够复制基本类型", () => {
+    const n = 123;
+    const n2 = deepClone(n);
+    assert(n === n2);
+    const s = "123";
+    const s2 = deepClone(s);
+    assert(s === s2);
+    const b = true;
+    const b2 = deepClone(b);
+    assert(b === b2);
+    const u = undefined;
+    const u2 = deepClone(u);
+    assert(u === u2);
+    const null1 = null;
+    const null2 = deepClone(null1);
+    assert(null1 === null2);
+    // const symbol1 = new Symbol();
+    // const symbol2 = deepClone(symbol1);
+    // assert(symbol1 === symbol2);
+  });
+  describe("对象", () => {
+    it("能够复制普通对象", () => {
+      const a = { name: "haha", child: { name: "lala" } };
+      const a2 = deepClone(a);
+      assert(a !== a2);
+      assert(a.name === a2.name);
+      assert(a.child !== a2.child);
+      assert(a.child.name === a2.child.name);
+    });
+    it("能够复制数组对象", () => {
+      const a = [[1, "2"], [3, 4]];
+      const a2 = deepClone(a);
+      assert(a !== a2);
+      assert(a[0] !== a2[0]);
+      assert(a[1] !== a2[1]);
+      assert.deepEqual(a, a2);
+    });
+    it("能够复制函数", () => {
+      const a = function() {
+        return 1;
+      };
+      a.xxx = { yyy: { zzz: 1 } };
+      const a2 = deepClone(a);
+      assert(a !== a2);
+      assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz);
+      assert(a.xxx.yyy !== a2.xxx.yyy);
+      assert(a.xxx !== a2.xxx);
+      assert(a() === a2());
+    });
+  });
+});
