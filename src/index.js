bc12f965018bc37730e3abb75bfeed9a5b2e6a96
wanghuan <196912334@qq.com>
2019-09-07 23:39:11 +0800

    通过面向对象写法仅保存单个实例的cache

--- src/index.js
+++ src/index.js
@@ -1,47 +1,51 @@
-let cache = [];
+class DeepClone {
+  constructor() {
+    this.cache = [];
+  }
 
-function deepClone(source) {
-  if (source instanceof Object) {
-    // 判断是否有缓存
-    let cacheDist = findCache(source);
-    if (cacheDist) {
-      // console.log("有缓存");
-      return cacheDist;
-    } else {
-      // console.log("无缓存");
-      let dist;
-      if (source instanceof Array) {
-        dist = new Array();
-      } else if (source instanceof Function) {
-        dist = function() {
-          return source.apply(this, arguments);
-        };
-      } else if (source instanceof RegExp) {
-        dist = new RegExp(source.source, source.flags);
-      } else if (source instanceof Date) {
-        dist = new Date(source);
+  clone(source) {
+    if (source instanceof Object) {
+      // 判断是否有缓存
+      let cacheDist = this.findCache(source);
+      if (cacheDist) {
+        // console.log("有缓存");
+        return cacheDist;
       } else {
-        dist = new Object();
-      }
-      cache.push([source, dist]);
-      for (let key in source) {
-        if (source.hasOwnProperty(key)) {
-          dist[key] = deepClone(source[key]);
+        // console.log("无缓存");
+        let dist;
+        if (source instanceof Array) {
+          dist = new Array();
+        } else if (source instanceof Function) {
+          dist = function() {
+            return source.apply(this, arguments);
+          };
+        } else if (source instanceof RegExp) {
+          dist = new RegExp(source.source, source.flags);
+        } else if (source instanceof Date) {
+          dist = new Date(source);
+        } else {
+          dist = new Object();
         }
+        this.cache.push([source, dist]);
+        for (let key in source) {
+          if (source.hasOwnProperty(key)) {
+            dist[key] = this.clone(source[key]);
+          }
+        }
+        return dist;
       }
-      return dist;
     }
+    return source;
   }
-  return source;
+
+  findCache(source) {
+    for (let i = 0; i < this.cache.length; i++) {
+      if (this.cache[i][0] === source) {
+        return this.cache[i][1];
+      }
+    }
+    return undefined;
+  }
 }
 
-function findCache(source) {
-  for (let i = 0; i < cache.length; i++) {
-    if (cache[i][0] === source) {
-      return cache[i][1];
-    }
-  }
-  return undefined;
-}
-
-module.exports = deepClone;
+module.exports = DeepClone;
--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
 const chai = require("chai");
-const deepClone = require("../src/index");
+const DeepClone = require("../src/index");
 const assert = chai.assert;
+
+// const deepClone = new DeepClone().clone;
 
 describe("深拷贝", () => {
   it("deepClone是个函数", () => {
@@ -29,7 +31,7 @@ describe("深拷贝", () => {
   describe("对象", () => {
     it("能够复制普通对象", () => {
       const a = { name: "haha", child: { name: "lala" } };
-      const a2 = deepClone(a);
+      const a2 = new DeepClone().clone(a);
       assert(a !== a2);
       assert(a.name === a2.name);
       assert(a.child !== a2.child);
@@ -37,7 +39,7 @@ describe("深拷贝", () => {
     });
     it("能够复制数组对象", () => {
       const a = [[1, "2"], [3, 4]];
-      const a2 = deepClone(a);
+      const a2 = new DeepClone().clone(a);
       assert(a !== a2);
       assert(a[0] !== a2[0]);
       assert(a[1] !== a2[1]);
@@ -48,7 +50,7 @@ describe("深拷贝", () => {
         return 1;
       };
       a.xxx = { yyy: { zzz: 1 } };
-      const a2 = deepClone(a);
+      const a2 = new DeepClone().clone(a);
       assert(a !== a2);
       assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz);
       assert(a.xxx.yyy !== a2.xxx.yyy);
@@ -58,7 +60,7 @@ describe("深拷贝", () => {
     it("能够复制环", () => {
       const a = { name: "haha" };
       a.self = a;
-      const a2 = deepClone(a);
+      const a2 = new DeepClone().clone(a);
       assert(a !== a2);
       assert(a.name === a2.name);
       assert(a.self !== a2.self);
@@ -73,7 +75,7 @@ describe("深拷贝", () => {
         };
         b = b.child;
       }
-      const a2 = deepClone(a);
+      const a2 = new DeepClone().clone(a);
       assert(a !== a2);
       assert(a.name === a2.name);
       assert(a.child !== a2.child);
@@ -81,7 +83,7 @@ describe("深拷贝", () => {
     it("能够复制正则", () => {
       const a = new RegExp("hid+", "gi");
       a.xxx = { yyy: { zzz: 1 } };
-      const a2 = deepClone(a);
+      const a2 = new DeepClone().clone(a);
       assert(a.source === a2.source);
       assert(a.flags === a2.flags);
       assert(a !== a2);
@@ -92,7 +94,7 @@ describe("深拷贝", () => {
     it("能够复制Date", () => {
       const a = new Date();
       a.xxx = { yyy: { zzz: 1 } };
-      const a2 = deepClone(a);
+      const a2 = new DeepClone().clone(a);
       assert(a.getTime() === a2.getTime());
       assert(a !== a2);
       assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz);
@@ -102,12 +104,69 @@ describe("深拷贝", () => {
     it("自动跳过原型", () => {
       const a = Object.create({ name: "haha" });
       a.xxx = { yyy: { zzz: 1 } };
-      const a2 = deepClone(a);
+      const a2 = new DeepClone().clone(a);
       assert.isFalse("name" in a2);
       assert(a !== a2);
       assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz);
       assert(a.xxx.yyy !== a2.xxx.yyy);
       assert(a.xxx !== a2.xxx);
+    });
+    it("很复杂的对象", () => {
+      const a = {
+        n: NaN,
+        n2: Infinity,
+        s: "",
+        bool: false,
+        null: null,
+        u: undefined,
+        sym: Symbol(),
+        o: {
+          n: NaN,
+          n2: Infinity,
+          s: "",
+          bool: false,
+          null: null,
+          u: undefined,
+          sym: Symbol()
+        },
+        array: [
+          {
+            n: NaN,
+            n2: Infinity,
+            s: "",
+            bool: false,
+            null: null,
+            u: undefined,
+            sym: Symbol()
+          }
+        ]
+      };
+      const a2 = new DeepClone().clone(a);
+      assert(a !== a2);
+      assert.isNaN(a2.n);
+      assert(a.n2 === a2.n2);
+      assert(a.s === a2.s);
+      assert(a.bool === a2.bool);
+      assert(a.null === a2.null);
+      assert(a.u === a2.u);
+      assert(a.sym === a2.sym);
+      assert(a.o !== a2.o);
+      assert.isNaN(a2.o.n);
+      assert(a.o.n2 === a2.o.n2);
+      assert(a.o.s === a2.o.s);
+      assert(a.o.bool === a2.o.bool);
+      assert(a.o.null === a2.o.null);
+      assert(a.o.u === a2.o.u);
+      assert(a.o.sym === a2.o.sym);
+      assert(a.array !== a2.array);
+      assert(a.array[0] !== a2.array[0]);
+      assert.isNaN(a2.array[0].n);
+      assert(a.array[0].n2 === a2.array[0].n2);
+      assert(a.array[0].s === a2.array[0].s);
+      assert(a.array[0].bool === a2.array[0].bool);
+      assert(a.array[0].null === a2.array[0].null);
+      assert(a.array[0].u === a2.array[0].u);
+      assert(a.array[0].sym === a2.array[0].sym);
     });
   });
 });
